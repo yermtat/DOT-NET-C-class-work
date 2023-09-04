3d07c2b14b954f5861ecf798524346948398d363
yermtat <yerm.tat@gmail.com>
2023-09-04 17:44:31 +0400

    inheritance polymorphism

---
+++ inheritance/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+Derived derived = new Derived("Salam", 13, 46);
+derived.Show();
+derived.DerivedFoo();
+//GrandChild grand = new();
+//grand.Show();
+
+GrandChild grandChild = new();
+
+class Base
+{
+    public int Field1 { get; set; }
+    private int Field2;
+
+    public Base()
+    {
+        Field1 = 1;
+        Field2 = 2;
+        Console.WriteLine("Base default constructor");
+    }
+    public Base(int field1, int field2)
+    {
+        Field1 = field1;
+        Field2 = field2;
+        Console.WriteLine("Base parameterized constructor");
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Field1 = {Field1}, Field2 = {Field2}");
+    }
+
+    protected void Foo()
+    {
+        Console.WriteLine("Protected method Foo");
+    }
+
+    private void Bar()
+    {
+        Console.WriteLine("Private method Bar");
+    }
+}
+
+class Derived : Base
+{
+    public string SomeProperty { get; set; }
+
+    public Derived()
+    {
+        Console.WriteLine("Derived default constructor");
+    }
+
+    public Derived(string some, int field1, int field2) : base(field1, field2) // вызывает конструктор родительского класса 
+    {
+        SomeProperty = some;
+        Console.WriteLine("Derived parameterized constructor");
+    }
+    public void DerivedFoo()
+    {
+        Foo(); // метод фу видно только внутри дочернего класса, т.к. он протектед, объектам дочернего класса этот метод не доступен
+
+    }
+}
+
+// здесь нет множественного наследования, класс может наследоваться только от одного другого класса 
+
+//class GrandChild : Base, Derived      // ошибка компиляции
+//{
+
+//}
+
+
+sealed /*дописываем если не хотим чтобы от этого класса еще что-то наследовалось*/class GrandChild : Derived
+{
+    public GrandChild() : base("Hi", 36, 89)
+    {
+        Console.WriteLine("Grandchild default constructor");
+    }
+
+    public GrandChild(string some, int field1, int field2) : base(some, field1, field2)
+    {
+        Console.WriteLine("Grandchild parameterized constructor");
+    }
+}
+
+//class Kolbas : Grandchild     // ошибка компиляции т.к. не может наследоваться от sealed класса
+//{
+
+//}
---
+++ polymorphism/Program.cs
@@ -0,0 +1,148 @@
+﻿//Base @base = new(); // если нужно использовать какое-либо ключевое слово как имя для переменной ставится @ перед переменной
+
+Base myBase = new();
+//myBase.Show();
+//Console.WriteLine(myBase.Field1);
+Derived derived = new();
+//Console.WriteLine(derived.Field1);
+//derived.Show();
+
+Base derive = new Derived();
+//Console.WriteLine(derive.Field1);
+//((Derived)derive).Show();
+
+//Foo(myBase);
+//Foo(derive);
+//Foo(derived);
+//Foo(new Child());
+
+//derived.Print();
+
+
+//Console.WriteLine(derive); // по умолчанию вызывается ту стринг, который по умолчанию возвращает название класса, если его не переопределить
+
+Human atilla = new("Atilla", "Ismail", 14);
+Human nadir = new("Nadir", "Zamanov", 42);
+var value = atilla.ToString();
+Console.WriteLine(value);
+Console.WriteLine(nadir);
+
+
+void Foo(Base @base) 
+{
+    #region version 1
+    //// version 1
+    //if (@base.GetType() == new Derived().GetType())
+    //{ 
+    //    ((Derived)@base).Show();
+    //}
+    //if (@base.GetType() == new Child().GetType())
+    //{
+    //    ((Child)@base).Show();
+    //}
+    //else
+    //{
+    //    @base.Show();
+    //}
+    #endregion
+
+
+    //try
+    //{
+    //    ((Derived)@base).Show();
+    //}
+
+    #region version 3
+    //if (@base is Derived)
+    //{
+    //    ((Derived)@base).Show();
+    //}
+    //if (@base is Child)
+    //{
+    //    ((Child)@base).Show();
+    //}
+    //else
+    //{
+    //    @base.Show();
+    //}
+    #endregion
+
+
+    #region version 4
+
+    //var value = @base as Derived;
+    //var value1 = @base as Child;
+    //var value2 = @base as Base;
+    //if (value != null) value.Show();
+    //if (value1 != null) value.Show();
+    //if (value2 != null) value.Show();
+    #endregion
+
+    @base.Show();
+
+}
+
+class Base
+{
+    public string Field1 { get; set; } = "Salam";
+    public virtual void Show()
+    {
+        Console.WriteLine("Base class show");
+    }
+}
+
+class Derived : Base
+{
+    public new string Field1 { get; set; } = "Popolam";
+    public override void Show()
+    {
+        Console.WriteLine("Derived class show");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(Field1);
+        base.Show();
+    }
+
+    public override string ToString()
+    {
+        return "nya";
+    }
+}
+
+
+class Child : Base
+{
+    public override void Show()
+    {
+        Console.WriteLine("Child class show");
+    }
+    
+}
+
+
+class Human
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public Human(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $@"Name: {Name} 
+Surname: {Surname}
+Age: {Age}";
+    }
+}
+
+
+
+// прочитать про интерфейсы и абстрактные классы 
