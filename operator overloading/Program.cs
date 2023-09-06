82e1856c4a6a738059ba15f7626efa27c4a64d0c
yermtat <yerm.tat@gmail.com>
2023-09-06 18:07:41 +0400

    operator overloading

---
+++ operator overloading/Program.cs
@@ -0,0 +1,139 @@
+﻿Point point = new(x: 52, y: 36);
+Point point1 = new Point() { X = 0, Y = 0 };
+Console.WriteLine(point1.Equals(point));
+
+if (point1)
+{
+    Console.WriteLine("Not on coordinates start");
+}
+else
+{
+    Console.WriteLine("On coordinates start");
+}
+
+//Console.WriteLine(point++);
+//Console.WriteLine(point + point1);
+//Console.WriteLine(point1 * 5);
+
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"Pint (X : {X},  Y :{Y})";
+    }
+    public Point()
+    {
+        X = default;
+        Y = default;
+    }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Point operator ++(Point p)  // перегрузки операторов должны быть паблик и статическими
+    {
+        p.X++;
+        p.Y++;
+        return p;
+    }
+
+    public static Point operator --(Point p) // не нужно отдельно перегружать префиксную и постфиксную форму
+    {  
+        p.X--; 
+        p.Y--;
+        return p;
+    }
+
+    public static Point operator -(Point p)
+    {
+        return new Point(-p.X, -p.Y);
+    }
+
+    public static Point operator +(Point left, Point right)
+    {
+        return new Point { X = left.X + right.X, Y = left.Y + right.Y };
+    }
+
+    public static Point operator -(Point left, Point right)
+    {
+        return new Point { X = left.X - right.X, Y = left.Y - right.Y };
+    }
+
+    public static Point operator *(Point left, int value)
+    {
+        return new Point { X = left.X *value, Y = left.Y * value };
+    }
+
+    public static Point operator *( int value,Point left)
+    {
+        return new Point { X = left.X * value, Y = left.Y * value };
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return this.ToString() == obj.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        return this.ToString().GetHashCode(); 
+    }
+
+    // операторы сравнения должны писаться парами
+
+    public static bool operator == (Point left, Point right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Point left, Point right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator > (Point left, Point right)
+    {
+        return Math.Sqrt(Math.Pow(left.X, 2) + Math.Pow(left.Y, 2)) >
+            Math.Sqrt(Math.Pow(right.X, 2) + Math.Pow(right.Y, 2));
+    }
+
+    public static bool operator <(Point left, Point right)
+    {
+        return Math.Sqrt(Math.Pow(left.X, 2) + Math.Pow(left.Y, 2)) <
+            Math.Sqrt(Math.Pow(right.X, 2) + Math.Pow(right.Y, 2));
+    }
+
+    public static bool operator true(Point point)
+    {
+        return point.X != 0 || point.Y !=0 ? true : false;
+    }
+
+    public static bool operator false(Point point)
+    {
+        return point.X == 0 && point.Y == 0 ? true : false;
+    }
+
+    public static Point operator |(Point left, Point right)
+    {
+        if ((left.X != 0 || left.Y != 0) || (right.X != 0 || right.Y != 0))
+        {
+            return right;
+        }
+        return new Point();
+    }
+
+    public static Point operator &(Point left, Point right)
+    {
+        if ((left.X != 0 && left.Y != 0) && (right.X != 0 && right.Y != 0))
+        {
+            return right;
+        }
+        return new Point();
+    }
+}
---
+++ typecasting overload/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Rectangle rectangle = new Rectangle() { Width = 4, Height = 4 };
+Square square = new Square { Length = 5 };
+rectangle.Draw();
+square.Draw();
+
+Rectangle rectSquare = square;
+rectSquare.Draw();
+
+Square squareRect = (Square)rectangle;
+squareRect.Draw();
+
+int number = int.Parse(Console.ReadLine());
+Square intSquare = number;
+intSquare.Draw();
+
+Console.WriteLine((int)intSquare);
+abstract class Figure
+{
+    public abstract void Draw();
+}
+
+abstract class Quadranangle : Figure { }
+
+class Rectangle : Quadranangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public static implicit operator Rectangle(Square s)
+    {
+        return new Rectangle 
+        { 
+            Height = s.Length, 
+            Width = s.Length* 2
+        };
+    }
+
+    public override void Draw()
+    {
+        for (int i = 0; i <Height ; i++, Console.WriteLine())
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Console.Write("* ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+class Square : Quadranangle
+{
+    public int Length { get; set; }
+
+    public static explicit operator Square(Rectangle rectangle)
+    {
+        return new Square { Length = rectangle.Height };
+    }
+    public static explicit operator int(Square square)
+    {
+        return square.Length;
+    }
+
+    public static implicit operator Square(int number)
+    {
+        return new Square { Length = number }; 
+    }
+    public override void Draw()
+    {
+        for (int i = 0; i < Length; i++, Console.WriteLine())
+        {
+            for (int j = 0; j < Length; j++)
+            {
+                Console.Write("* ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// нельзя неявно преобразовывать со старшего типа в младший с потерей данных, и вообще нельзя преобразовать на бул явно или неявно
