2775539f0f97ede16353d349b3a2177ffd77dff7
yermtat <yerm.tat@gmail.com>
2023-09-21 17:40:55 +0400

    lesson 14

---
+++ multicast delegate/Program.cs
@@ -0,0 +1,6 @@
+﻿// mulicast delegate
+
+string? name = null; // ? явно указывает наллбл тип
+// если ? после переменной но до точки до метода - проверка на то налл или нет
+Console.WriteLine(name?.Length);
+
