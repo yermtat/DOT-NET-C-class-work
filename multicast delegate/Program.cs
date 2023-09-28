1bf549887101161efebeeffe463386afd73c53e3
yermtat <yerm.tat@gmail.com>
2023-09-28 17:47:58 +0400

    repeat

--- multicast delegate/Program.cs
+++ multicast delegate/Program.cs
@@ -4,3 +4,12 @@ string? name = null; // ? явно указывает наллбл тип
 // если ? после переменной но до точки до метода - проверка на то налл или нет
 Console.WriteLine(name?.Length);
 
+name ??= "Elvin"; // если нейм налл запиши туда эльвин
+
+
+string foo(string text)
+{
+    return text ?? throw new ArgumentNullException(nameof(text)); // если текст налл брось эксепшн если нет ретерн налл
+}
+
+// можно скзать ?? это тернарный оператор специально для налл случаев
